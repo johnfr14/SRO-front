0942c879be3dd23e022eb0a1f2c266dab6ab1a83
Imo999 <rcajina.pro@gmail.com>
2021-08-07 18:54:35 +0200

    update : need ajust page

--- src/components/CreateNftPage/Erc721Nft.js
+++ src/components/CreateNftPage/Erc721Nft.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { PreviewFile, NftPrice, UnlockToggle, UploadFile } from '.'
 
 const Erc721Nft = () => {
   return (
-    <div className="text-white">
-      Interface ERC721
+    <div className="min-h-screen pt-2 font-mono my-16 ">
+      <div className="container mx-auto">
+        <h2 className="text-5xl text-white text-center font-bold pb-10">Create single collectible</h2>
+        <UploadFile />
+        <PreviewFile />
+        <NftPrice />
+        <UnlockToggle />
+      </div>
+
+
+
     </div>
   )
 }
