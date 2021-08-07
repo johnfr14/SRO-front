fcf3cf4dfd7eda992a0162020f70675ebeb40c49
Imo999 <rcajina.pro@gmail.com>
2021-08-07 18:53:15 +0200

    Update Index components

--- src/components/CreateNftPage/index.js
+++ src/components/CreateNftPage/index.js
@@ -1,3 +1,7 @@
 import Erc721Nft from "./Erc721Nft";
+import NftPrice from "./NftPrice";
+import PreviewFile from "./PreviewFile";
+import UnlockToggle from "./UnlockToggle";
+import UploadFile from "./UploadFile";
 
-export { Erc721Nft };
+export { Erc721Nft, NftPrice, PreviewFile, UnlockToggle, UploadFile };
