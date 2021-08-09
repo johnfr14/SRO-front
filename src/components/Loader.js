88bc51586b6f020b82e8334f02a525ad53a11560
Imo999 <rcajina.pro@gmail.com>
2021-08-09 17:15:41 +0200

    add loader

---
+++ src/components/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../css/loader.css"
+
+const Loader = () => {
+  return (
+    <div>
+      <div className="svg-loader">
+        <svg className="svg-container" height="100" width="100" viewBox="0 0 100 100">
+          <circle className="loader-svg bg" cx="50" cy="50" r="45"></circle>
+          <circle className="loader-svg animate" cx="50" cy="50" r="45"></circle>
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
