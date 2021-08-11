bc7fbcd142fdb116f2cc64f86873468b3a447760
Imo999 <rcajina.pro@gmail.com>
2021-08-11 13:24:50 +0200

    Update Switch architecture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 import { CreateErc721, EditProfile, HomePage, OwnedPage } from "./pages";
+import test from "./pages/test"
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/user" component={OwnedPage} />
         <Route exact path="/settings" component={EditProfile} />
         <Route exact path="/create/erc721" component={CreateErc721} />
+        <Route exact path="/test" component={test} />
         <Redirect to="/" />
       </Switch>
     </>
