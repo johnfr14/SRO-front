19db904fe1d0a48ae11660e56618a258188a4932
Imo999 <rcajina.pro@gmail.com>
2021-08-13 00:29:01 +0200

    Update index

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import { AvatarSettings, SettingsInfo } from "./EditProfilePage/index"
-import { BannerInfos, TabZone } from "./UserMainPage/index";
+import { BannerInfos, TabZone, Noitems } from "./UserMainPage";
 import { Button, ButtonModal } from "./Button";
-import Card from "./Card/Card";
+import { Card, CollectionCard, CreatorCard, DotMenu, MediaCard, ProfilList, OwnerCard } from "./Card/";
 import Container from "./Container";
 import Divider from "./Divider";
 import { Erc721Nft, PreviewFile, UploadFile } from "./CreateNftPage"
@@ -11,6 +11,7 @@ import Header from "./Header";
 import Hero from "./Hero";
 import LoaderIcon from "./LoaderIcon";
 import { Login } from "./Login";
+import { NftBuy, NftViewBuy, TabZoneBuyNft } from "./BuyNftPage/index"
 import TokenPrice from "./TokenPrice";
 import SwitchToggle from "./SwitchToggle"
 
@@ -21,19 +22,29 @@ export {
   ButtonModal,
   Card,
   Container,
+  CollectionCard,
+  CreatorCard,
   Divider,
+  DotMenu,
   Erc721Nft,
-  ModCheckout,
-  ModFixedPrice,
-  ModFollowStepCreate,
-  ModFollowStepSale,
-  ModPurchase,
   Footer,
   Header,
   Hero,
   LoaderIcon,
   Login,
+  MediaCard,
+  ModCheckout,
+  ModFixedPrice,
+  ModFollowStepCreate,
+  ModFollowStepSale,
+  ModPurchase,
+  NftBuy,
+  NftViewBuy,
+  Noitems,
+  OwnerCard,
   PreviewFile,
+  ProfilList,
+  TabZoneBuyNft,
   TokenPrice,
   SettingsInfo,
   SwitchToggle,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
+import BuyNft from "./BuyNft";
 import { CreateErc721 } from "./CreateNFT"
 import EditProfile from "./EditProfile";
 import HomePage from "./Home";
 import OwnedPage from "./OwnedPage";
 
-export { CreateErc721, EditProfile, HomePage, OwnedPage };
+export { BuyNft, CreateErc721, EditProfile, HomePage, OwnedPage };
