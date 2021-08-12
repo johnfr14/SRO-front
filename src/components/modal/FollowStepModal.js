95594e2a777001a892cf19be9da17e24eb50f10b
Imo999 <rcajina.pro@gmail.com>
2021-08-12 12:10:44 +0200

    add components

---
+++ src/components/modal/FollowStepModal.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Button, Loader } from '../'
+import { deleteIcon, checkmarkIcon } from "../../images/";
+
+const FollowStepModal = () => {
+  return (
+    <div className="bg-gradient-to-b from-black to-gray-700 flex justify-center items-center py-5">
+      <div className="bg-gray-700 rounded-lg">
+        <div className="w-96 border-t-8 border-b-8 border-yellow-300 rounded-lg">
+          <div className="">
+            <div className="text-white text-center">
+              <h2 className="text-5xl font-bold py-4">Follow steps</h2>
+              <h3 className="text-3xl font-bold">Mint</h3>
+              <p className="text-xs">Send transaction to create your NFT</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center pt-4 pb-3 pr-5 ">
+            <div className=" pr-5 ">
+              <img alt="" className="w-7 " src={deleteIcon} />
+            </div>
+            <div className="pr-5">
+              <Button target={""} buttonStyle>Try again</Button>
+            </div>
+          </div>
+          <div className="">
+            <div className="text-white text-center">
+              <h3 className="text-3xl font-bold">Approve</h3>
+              <p className="text-xs">This transaction is conducted only once per collection</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center pt-4 pb-3 pr-5 ">
+            <div className=" pr-5 ">
+              <img alt="" className="w-7 " src={checkmarkIcon} />
+            </div>
+            <div className="pr-5">
+              <Button target={""} buttonStyle>Start</Button>
+            </div>
+          </div>
+          <div className="">
+            <div className="text-white text-center">
+              <h3 className="text-3xl font-bold">Set fixed price</h3>
+              <p className="text-xs">Sign message to set fixed price</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center pt-4 pb-3 pr-5 ">
+            <Loader />
+            <div className="pr-5">
+              <Button target={""} buttonStyle>In-progress</Button>
+            </div>
+          </div>
+          <div className="flex items-center justify-center pt-3 pb-3">
+            <a href="#1" className="  px-5 py-3 text-center bg-gray-400 text-white hover:bg-gray-200 hover:text-black font-bold rounded-lg text-sm">Cancel</a>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default FollowStepModal
