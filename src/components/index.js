7c60a8a200951654da5922e2955adfeefc732d81
Imo999 <rcajina.pro@gmail.com>
2021-08-12 13:09:55 +0200

    update

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Card from "./Card/Card";
 import Container from "./Container";
 import Divider from "./Divider";
 import { Erc721Nft, NftPrice, PreviewFile, UploadFile } from "./CreateNftPage"
-import { ModFollowStepCreate } from "./modal";
+import { ModFollowStepCreate } from "./Modal";
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
