a53c5985d5f222d3c0cffc1948a5676b4b733302
Imo999 <rcajina.pro@gmail.com>
2021-08-13 13:49:18 +0200

    add component NftSales

--- src/components/SaleNftPage/TabZoneSaleNft.js
+++ src/components/SaleNftPage/TabZoneSaleNft.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import { useState } from "react";
+import { Tab } from "@headlessui/react";
+
+import "../../css/userTab.css";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 
 const TabZoneSaleNft = () => {
+
+  let [categories] = useState({
+    Owners: [
+      {
+        id: 1,
+        name: "Owners",
+        component: "Coming Soon",
+      },
+    ],
+    Bids: [
+      {
+        id: 1,
+        name: "Bids",
+        component: "Coming Soon",
+      },
+    ],
+    Details: [
+      {
+        id: 1,
+        name: "Détails",
+        component: "Coming Soon",
+      },
+    ],
+    History: [
+      {
+        id: 1,
+        name: "History",
+        component: "Coming Soon",
+      },
+    ],
+  });
+
   return (
-    <div>
-      Interface TabZoneSaleNft
+    <div className="">
+      <Tab.Group>
+        <div className="flex">
+          <Tab.List className="hPGlPu lhlpVE space-x-14">
+            {Object.keys(categories).map((category) => (
+              <Tab
+                key={category}
+                className={({ selected }) =>
+                  classNames(
+                    "py-2.5 text-xl  leading-5 font-medium text-white rounded-lg w-auto relative",
+                    "",
+                    selected
+                      ? "selected"
+                      : "text-gray-600 hover:bg-white/[0.12] hover:text-white"
+                  )
+                }
+              >
+                <span>{category}</span>
+                <div className="cAtPEH" />
+              </Tab>
+            ))}
+          </Tab.List>
+        </div>
+
+        <Tab.Panels className="mt-2">
+          {Object.values(categories).map((posts, idx) => (
+            <Tab.Panel key={idx} className={classNames("text-white text-center py-2")}>
+              <ul>
+                {posts.map((post) => (
+                  <li
+                    key={post.id}
+                    className="relative p-3 rounded-md hover:bg-coolGray-100"
+                  >
+                    <h3 className="text-sm font-medium leading-5">
+                      {post.component}
+                    </h3>
+                  </li>
+                ))}
+              </ul>
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   )
 }
