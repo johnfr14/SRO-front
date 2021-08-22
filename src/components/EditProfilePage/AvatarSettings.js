c102990ddd9164f1690a9b8acf069e4088f1ef43
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-08-22 18:14:41 +0200

    Fix and add responsive on settings page

--- src/components/EditProfilePage/AvatarSettings.js
+++ src/components/EditProfilePage/AvatarSettings.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { userDefault } from "../../images";
 import classnames from "classnames";
 
-const AvatarSettings = ({ register, watch, avatar}) => {
-  const [preview, setPreview] = useState(null)
+const AvatarSettings = ({ register, watch, avatar }) => {
+  const [preview, setPreview] = useState(null);
 
   useEffect(() => {
-    
     if (watch.length > 0) {
-      setPreview(URL.createObjectURL(watch[0]))
+      setPreview(URL.createObjectURL(watch[0]));
     }
-  }, [watch])
+  }, [watch]);
 
   return (
     <>
-      <div className="max-w-xs bg-gray-900 shadow-lg rounded-xl p-2 mt-3 border-2 border-gray-200 border-opacity-25 pb-3 relative">
+      <div className="max-w-xs bg-gray-900 shadow-lg rounded-xl p-2 mt-3 border-2 border-gray-200 border-opacity-25 pb-3">
         <div className="flex flex-col items-center ">
           <img
             className="flex justify-center px-4 py-5 w-48 "
@@ -25,16 +24,26 @@ const AvatarSettings = ({ register, watch, avatar}) => {
         <div className="flex flex-col ">
           <div className="relative h-62 w-full mb-3 p-2">
             <div className="text-center pt-5">
-              <input 
-                    style={{display: "none"}}
-                    type="file"
-                    id="avatar"
-                    name="avatar"
-                    accept="image/png, image/jpeg"
-                    {...register("avatar")}
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="avatar"
+                name="avatar"
+                accept="image/png, image/jpeg"
+                {...register("avatar")}
               />
-              <label className={classnames("transition duration-300 bg-gradient-to-br rounded-xl hover:opacity-75")} style={{cursor: "pointer"}} htmlFor="avatar">Select Avatar</label>
-              <p className="mt-3 text-sm text-white pt-5" >
+              <label
+                className={classnames(
+                  "transition duration-300 bg-gradient-to-br rounded-xl hover:opacity-75",
+                  "text-black px-8 py-3 from-primary-200 to-primary-200" ||
+                    "text-white hover:text-primary-200"
+                )}
+                style={{ cursor: "pointer" }}
+                htmlFor="avatar"
+              >
+                Select Avatar
+              </label>
+              <p className="mt-3 text-sm text-white pt-5">
                 We recommend an image of at least 400x400.
               </p>
             </div>
@@ -42,7 +51,7 @@ const AvatarSettings = ({ register, watch, avatar}) => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AvatarSettings
+export default AvatarSettings;
--- src/components/EditProfilePage/SettingsInfo.js
+++ src/components/EditProfilePage/SettingsInfo.js
@@ -5,7 +5,6 @@ import { useUser } from "../../context/UserContext";
 import { Web3Context } from "web3-hooks";
 import classnames from "classnames";
 import axios from "axios";
-import { Button } from "../index";
 
 // @TODO: toast pour l'update du profil ( dans la fonction "onSubmit()")
 const SettingsInfo = ({ data }) => {
@@ -47,123 +46,122 @@ const SettingsInfo = ({ data }) => {
 
   return (
     <div className="mb-20">
-      <div className="container mx-auto">
-        <h2 className="text-5xl text-white text-center font-bold ">
-          Edit Profile
-        </h2>
-        <p className="text-white text-center my-8  pb-10 ">
-          You can set preferred display name, create your branded profile URL
-          and manage other personal settings
-        </p>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className=" relative h-full flex flex-col bg-gray-900 shadow-xl rounded-md mt-3 border-2 border-gray-200 border-opacity-25 pb-3"
-        >
-          {data.id && (
-            <div className="flex  items-center">
-              <AvatarSettings
-                register={register}
-                watch={watch().avatar || 0}
-                avatar={data.avatar}
-              />
-              <div className="container mx-auto max-w-screen-lg h-full">
-                <div className=" ml-24 items-center justify-center">
-                  <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
-                    Display name
-                  </label>
-                  <div className="pr-24">
-                    <input
-                      className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
-                      type="text"
-                      placeholder="Enter your display name"
-                      defaultValue={data.username}
-                      value={watch().username}
-                      {...register("username")}
-                    />
-                  </div>
-                </div>
-                <div className=" ml-24 items-center justify-center">
-                  <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
-                    Bio
-                  </label>
-                  <div className="pr-24">
-                    <input
-                      className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
-                      type="text"
-                      placeholder="Tell about yourself in a few words"
-                      defaultValue={data.bio}
-                      value={watch().bio}
-                      {...register("bio")}
-                    />
-                  </div>
-                </div>
-                <div className=" ml-24 mr-24 items-center justify-center">
-                  <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
-                    Custom URL
-                  </label>
-                  <div className="flex flex-wrap items-stretch w-full mb-4 relative">
-                    <div className="flex -mr-px">
-                      <span className="flex items-center leading-normal bg-gray-600 text-white rounded border border-gray-600 shadow-inner py-3 px-5 whitespace-no-wrap text-grey-dark ">
-                        https://sarahro.io/users/
-                      </span>
+      <div className="">
+        <div>
+          <h2 className=" text-white text-center font-bold ">Edit Profile</h2>
+          <p className="text-white text-center my-8  pb-10 ">
+            You can set preferred display name, create your branded profile URL
+            and manage other personal settings
+          </p>
+        </div>
+        <div className="">
+          <form onSubmit={handleSubmit(onSubmit)} className="">
+            {data.id && (
+              <div className="flex flex-col md:flex-row justify-center">
+                <AvatarSettings
+                  register={register}
+                  watch={watch().avatar || 0}
+                  avatar={data.avatar}
+                />
+                <div className="md:ml-24">
+                  <div className="items-center ">
+                    <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+                      Display name
+                    </label>
+                    <div className="">
+                      <input
+                        className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
+                        type="text"
+                        placeholder="Enter your display name"
+                        defaultValue={data.username}
+                        value={watch().username}
+                        {...register("username")}
+                      />
                     </div>
-                    <input
-                      type="text"
-                      className="flex-shrink flex-grow flex-auto leading-normal w-px bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4 focus:outline-none  focus:border-gray-500"
-                      placeholder="Enter your custom URL"
-                      defaultValue={data.url}
-                      value={watch().url}
-                      {...register("url", { pattern: /^[A-Za-z0-9]+$/i })}
-                    />
-                    {errors.url &&
-                      "Wrong syntaxe, only alphabet character accepted (a)"}
                   </div>
-                </div>
-                <div className=" ml-24 mr-24 items-center justify-center">
-                  <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
-                    Twitter Username
-                  </label>
-                  <p className="block uppercase tracking-wide text-white text-xs mb-3">
-                    Link your Twitter account to gain more trust on the
-                    marketplace
-                  </p>
-                  <input
-                    className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
-                    type="text"
-                    placeholder="@pseudo"
-                    defaultValue={data.twitterUsername}
-                    value={watch().twitterUsername}
-                    {...register("twitterUsername")}
-                  />
-                </div>
-                <div className=" ml-24 mr-24 items-center justify-center">
-                  <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
-                    Personal site or portfolio
-                  </label>
-                  <input
-                    className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
-                    type="text"
-                    placeholder="https://"
-                    defaultValue={data.portfolio}
-                    value={watch().portfolio}
-                    {...register("portfolio")}
-                  />
-                </div>
-                <div className="flex items-center justify-center mb-5 mt-8">
-                  <input
-                    type="submit"
-                    className={classnames(
-                      "transition duration-300 bg-gradient-to-br rounded-xl hover:opacity-75",
-                      "text-black px-8 py-3 from-primary-200 to-primary-200" ||
-                        "text-white hover:text-primary-200"
-                    )}
-                    value="Update profile"
-                  />
+                  <div className="items-center justify-center">
+                    <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+                      Bio
+                    </label>
+                    <div className="">
+                      <input
+                        className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
+                        type="text"
+                        placeholder="Tell about yourself in a few words"
+                        defaultValue={data.bio}
+                        value={watch().bio}
+                        {...register("bio")}
+                      />
+                    </div>
+                  </div>
+                  <div className="items-center justify-center">
+                    <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+                      Custom URL
+                    </label>
+                    <div className="flex flex-col md:flex-row mb-4">
+                      <div className="flex -mr-px mb-2 md:mb-0">
+                        <span className="items-center leading-normal bg-gray-600 text-white rounded border border-gray-600 shadow-inner py-3 px-4 whitespace-no-wrap text-grey-dark ">
+                          https://sarahro.io/users/
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                        placeholder="Enter your custom URL"
+                        defaultValue={data.url}
+                        value={watch().url}
+                        {...register("url", { pattern: /^[A-Za-z0-9]+$/i })}
+                      />
+                      {errors.url &&
+                        "Wrong syntaxe, only alphabet character accepted (a)"}
+                    </div>
+                  </div>
+                  <div className="items-center justify-center">
+                    <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+                      Twitter Username
+                    </label>
+                    <p className="block uppercase tracking-wide text-white text-xs mb-3">
+                      Link your Twitter account to gain more trust on the
+                      marketplace
+                    </p>
+                    <input
+                      className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                      type="text"
+                      placeholder="@pseudo"
+                      defaultValue={data.twitterUsername}
+                      value={watch().twitterUsername}
+                      {...register("twitterUsername")}
+                    />
+                  </div>
+                  <div className="items-center justify-center">
+                    <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+                      Personal site or portfolio
+                    </label>
+                    <input
+                      className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                      type="text"
+                      placeholder="https://"
+                      defaultValue={data.portfolio}
+                      value={watch().portfolio}
+                      {...register("portfolio")}
+                    />
+                  </div>
+                  <div className="flex items-center justify-center mb-5 mt-8">
+                    <input
+                      type="submit"
+                      className={classnames(
+                        "transition duration-300 bg-gradient-to-br rounded-xl hover:opacity-75",
+                        "text-black px-8 py-3 from-primary-200 to-primary-200" ||
+                          "text-white hover:text-primary-200"
+                      )}
+                      value="Update profile"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-        </form>
+            )}
+          </form>
+        </div>
       </div>
     </div>
   );
