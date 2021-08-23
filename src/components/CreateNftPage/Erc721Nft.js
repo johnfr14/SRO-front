989f7de48fd7c497edee61ba9ee54a933877795c
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-08-23 15:02:22 +0200

    Clean and add responsive erc721 page

--- src/components/CreateNftPage/Erc721Nft.js
+++ src/components/CreateNftPage/Erc721Nft.js
@@ -7,18 +7,20 @@ const Erc721Nft = () => {
   const [isToggled, setIsToggled] = useState(false);
 
   return (
-    <div className="min-h-screen pt-2 font-mono my-16 ">
+    <div className="min-h-screen pt-2 font-mono my-16 text-white">
       <div className="container mx-auto">
-        <h2 className="text-5xl text-white text-center font-bold pb-10">
+        <h1 className="text-5xl  text-center font-bold pb-10">
           Create single collectible
-        </h2>
-        <div className="">
+        </h1>
+        <div className="mb-5">
           <UploadFile />
         </div>
-        <div className="flex items-center justify-center">
-          <PreviewFile />
+        <div className="flex flex-col md:flex-row items-center justify-center">
+          <div>
+            <PreviewFile />
+          </div>
           <div className="flex">
-            <div className="pl-5 text-white">
+            <div className="md:pl-5 ">
               <div className="">
                 <h2 className="text-2xl font-semibold pt-4 pb-1">
                   Put on marketplace
@@ -27,13 +29,13 @@ const Erc721Nft = () => {
                   isToggled={isToggledPrice}
                   onToggle={() => setIsToggledPrice(!isToggledPrice)}
                 />
-                <p className="block tracking-wide  text-xs mb-2 mt-2">
+                <p className="block tracking-wide text-xs mb-2 mt-2">
                   Put your new NFT on XSRO marketplace
                 </p>
                 <TokenPrice />
               </div>
               <div className="">
-                <h2 className="text-2xl text-white font-semibold pt-4 pb-1">
+                <h2 className="text-2xl font-semibold pt-4 pb-1">
                   Unlock once purchased
                 </h2>
                 <SwitchToggle
@@ -41,11 +43,11 @@ const Erc721Nft = () => {
                   onToggle={() => setIsToggled(!isToggled)}
                 />
                 <input
-                  className="appearance-none block mt-2 w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
+                  className="appearance-none block mt-2 w-full bg-gray-900  border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
                   type="text"
                   placeholder="Digital key, linkto file..."
                 />
-                <label className="block tracking-wide text-white text-xs mb-2 mt-2">
+                <label className="block tracking-wide  text-xs mb-2 mt-2">
                   Content will be unlocked after successful transaction
                 </label>
               </div>
@@ -53,44 +55,44 @@ const Erc721Nft = () => {
           </div>
         </div>
         <div className="container mx-auto max-w-screen-lg h-full">
-          <div className=" relative h-full flex flex-col bg-gray-900 shadow-xl rounded-md mt-3 border-2 border-gray-200 border-opacity-25 pb-3">
-            <div className=" ml-24 items-center justify-center">
-              <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+          <div className=" relative h-full flex flex-col bg-gray-900 shadow-xl rounded-md mt-3 border-2 border-gray-200 border-opacity-25 p-5 ">
+            <div className="items-center justify-center">
+              <label className="block uppercase tracking-wide  text-xl font-bold mb-2 mt-5">
                 Title
               </label>
-              <div className="pr-24">
+              <div className="">
                 <input
-                  className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
+                  className="appearance-none block w-full bg-gray-900  border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
                   type="text"
                   placeholder="Title NFT"
                 />
               </div>
               <p className="text-xs text-red-500 mt-2">"Title" is required</p>
             </div>
-            <div className=" ml-24 items-center justify-center">
-              <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+            <div className="items-center justify-center">
+              <label className="block uppercase tracking-wide  text-xl font-bold mb-2 mt-5">
                 Decription (Optional)
               </label>
-              <div className="pr-24">
+              <div className="">
                 <input
-                  className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
+                  className="appearance-none block w-full bg-gray-900 border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
                   type="text"
                   placeholder="Descriptif NFT"
                 />
               </div>
             </div>
-            <div className=" ml-24 items-center justify-center">
-              <label className="block uppercase tracking-wide text-white text-xl font-bold mb-2 mt-5">
+            <div className="items-center justify-center">
+              <label className="block uppercase tracking-wide  text-xl font-bold mb-2 mt-5">
                 Royalties
               </label>
-              <div className="pr-24">
+              <div className="">
                 <input
-                  className="appearance-none block w-full bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
+                  className="appearance-none block w-full bg-gray-900  border border-gray-400 shadow-inner rounded-md py-3 px-4  leading-tight focus:outline-none  focus:border-gray-500"
                   type="text"
                   placeholder="10 %"
                 />
               </div>
-              <p className="block tracking-wide text-white text-xs mb-2 mt-2">
+              <p className="block tracking-wide text-xs mb-2 mt-2">
                 Suggested: 0%, 10%, 20%, 30%. Maximum is 50%
               </p>
             </div>
