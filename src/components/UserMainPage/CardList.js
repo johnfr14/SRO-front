57175213e8afea7d8dc83628e379ec540c0527f2
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-08-30 19:47:54 +0200

    Add SubstrAdress onAdressCollection

--- src/components/UserMainPage/CardList.js
+++ src/components/UserMainPage/CardList.js
@@ -1,6 +1,10 @@
 import { Card, Noitems } from "../index";
 import { userTest } from "../../images/";
 
+const SubstrAdress = (dataAdress) => {
+  return dataAdress.substr(0, 6) + "..." + dataAdress.substr(-4);
+};
+
 const cardMediaTest =
   "https://img.rarible.com/prod/image/upload/t_preview/prod-itemImages/0xd07dc4262bcdbf85190c01c996b4c06a461d2430:8207";
 
@@ -54,34 +58,51 @@ const defaultData = [
 ];
 
 const CardList = ({ idx, data }) => {
-  console.log(`Tab: ${idx === 1 ? 'nftOnSale' : idx === 2 ? 'nft owned' : 'nft created'}`, data)
-  console.log(data.length > 0)
+  console.log(
+    `Tab: ${idx === 1 ? "nftOnSale" : idx === 2 ? "nft owned" : "nft created"}`,
+    data
+  );
+  console.log(data.length > 0);
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 md:gap-x-10 xl-grid-cols-4 gap-y-5 gap-x-6 ">
-      {data.length > 0 ?
-        <>
-          {data.map((data, index) => (
-            <Card
-              id={data.id}
-              key={data.id + index || defaultData.name + data.index}
-              imgUrl={data.metadata.url || defaultData.imgUrl}
-              name={data.metadata.title || defaultData.name}
-              amountLike={data.metadata.likes}
-              price={''}
-              unity={"eth"}
-              linkToNFT={defaultData.linkToNFT}
-              linkToProfilCollection={"SRO"}
-              linkToProfilCreator={<a href={`https://sarahro.io/user/${data.metadata.author}`}>{data.metadata.author}</a>}
-              linkToProfilOwner={<a href={`https://sarahro.io/user/${data.owner}`}>{data.owner}</a>}
-              userIconCollection={defaultData.imgUrl}
-              userIconCreator={data.creator.avatar}
-              userIconOwner={data.owner.avatar}
-              tipDataAdressCollection={"0xa4D174cF992ABf58A0E95D1f5A95443699640A8E"}
-              tipDataAdressCreator={data.creator.address}
-              tipDataAdressOwner={data.owner.address}
-            />))}
-        </> : <Noitems />}
+        {data.length > 0 ? (
+          <>
+            {data.map((data, index) => (
+              <Card
+                id={data.id}
+                key={data.id + index || defaultData.name + data.index}
+                imgUrl={data.metadata.url || defaultData.imgUrl}
+                name={data.metadata.title || defaultData.name}
+                amountLike={data.metadata.likes}
+                price={""}
+                unity={"eth"}
+                linkToNFT={defaultData.linkToNFT}
+                linkToProfilCollection={"SRO"}
+                linkToProfilCreator={
+                  <a href={`https://sarahro.io/user/${data.metadata.author}`}>
+                    {data.metadata.author}
+                  </a>
+                }
+                linkToProfilOwner={
+                  <a href={`https://sarahro.io/user/${data.owner}`}>
+                    {data.owner}
+                  </a>
+                }
+                userIconCollection={defaultData.imgUrl}
+                userIconCreator={data.creator.avatar}
+                userIconOwner={data.owner.avatar}
+                tipDataAdressCollection={SubstrAdress(
+                  "0xa4D174cF992ABf58A0E95D1f5A95443699640A8E"
+                )}
+                tipDataAdressCreator={data.creator.address}
+                tipDataAdressOwner={data.owner.address}
+              />
+            ))}
+          </>
+        ) : (
+          <Noitems />
+        )}
       </div>
     </>
   );
