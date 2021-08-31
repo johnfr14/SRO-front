6fa29d16294750219e264e3364e50a1d9002bcbe
johnfr14 <jonathan.alyra@gmail.com>
2021-08-31 16:14:48 +0200

    url

--- src/pages/OwnedPage.js
+++ src/pages/OwnedPage.js
@@ -1,14 +1,35 @@
 import Layout from "../components/Layout";
 import { Container, BannerInfos, TabZone } from "../components/index";
-import { useUser } from "../context/UserContext"
+import { userData } from "../data/fetchData"
+import { Web3Context } from "web3-hooks";
+import { useRouteMatch } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
 
 function OwnedPage() {
-  const { userState } = useUser()
+  const [web3State] = useContext(Web3Context); 
+  const [data, setData] = useState()
+  const match = useRouteMatch("/user/:address");
+
+  useEffect(() => {
+    const fetch = async() => {
+      const address = match.params.address.length === 42 ? match.params.address : web3State.account
+      console.log(address)
+      setData(await userData(address))
+    }
+
+    if (data === undefined || (data.address !== match.params.address && match.params.address === 42)) {
+      fetch()
+    }
+  }, [match, data, web3State])
+  
   return (
     <>
-      <Layout>
-        <Container children={<BannerInfos data={userState.data} />} />
-        {userState.data.address && <Container children={<TabZone user={userState.data}/>} />}
+       <Layout>
+        {data && 
+        <>
+          <Container children={<BannerInfos data={data} />} />
+          <Container children={<TabZone user={data}/>} /> 
+        </>}
       </Layout>
     </>
   );
