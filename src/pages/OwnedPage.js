30d529b96ff1a0d2b5900e9fb719081a93929c96
johnfr14 <jonathan.alyra@gmail.com>
2021-09-01 16:12:26 +0200

    reload data

--- src/pages/OwnedPage.js
+++ src/pages/OwnedPage.js
@@ -7,16 +7,15 @@ import { useState, useEffect, useContext } from "react";
 
 function OwnedPage() {
   const [web3State] = useContext(Web3Context); 
-  const [data, setData] = useState()
+  const [data, setData] = useState({address: null})
   const match = useRouteMatch("/user/:address");
-  console.log(match)
 
   useEffect(() => {
     const fetch = async() => {
       const address = match.params.address.length === 42 ? match.params.address : web3State.account
       setData(await userData(address.toLowerCase()))
     }
-    if (data === undefined || (data.address !== match.params.address && match.params.address.length === 41)) {
+    if (data === undefined || (data.fullAddress !== match.params.address)) {
       fetch()
     }
   }, [match.params.address, data, web3State])
@@ -24,7 +23,7 @@ function OwnedPage() {
   return (
     <>
        <Layout>
-        {data && 
+        {data.fullAddress === match.params.address && 
         <>
           <Container children={<BannerInfos data={data} />} />
           <Container children={<TabZone user={data}/>} /> 
