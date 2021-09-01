31e41e075904876ca7dfe731130ed2bc5099637b
johnfr14 <jonathan.alyra@gmail.com>
2021-09-01 16:47:11 +0200

    toLowerCase

--- src/pages/OwnedPage.js
+++ src/pages/OwnedPage.js
@@ -1,29 +1,29 @@
 import Layout from "../components/Layout";
 import { Container, BannerInfos, TabZone } from "../components/index";
 import { userData } from "../data/fetchData"
-import { Web3Context } from "web3-hooks";
+import { useUser } from "../context/UserContext"
 import { useRouteMatch } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 
 function OwnedPage() {
-  const [web3State] = useContext(Web3Context); 
+  const { userState } = useUser() 
   const [data, setData] = useState({address: null})
   const match = useRouteMatch("/user/:address");
 
   useEffect(() => {
     const fetch = async() => {
-      const address = match.params.address.length === 42 ? match.params.address : web3State.account
+      const address = match.params.address.length === 42 ? match.params.address : userState.fullAddress
       setData(await userData(address.toLowerCase()))
     }
-    if (data === undefined || (data.fullAddress !== match.params.address)) {
+    if (data === undefined || (data.fullAddress !== match.params.address.toLowerCase())) {
       fetch()
     }
-  }, [match.params.address, data, web3State])
+  }, [match.params.address, data, userState])
   
   return (
     <>
        <Layout>
-        {data.fullAddress === match.params.address && 
+        {data.fullAddress === match.params.address.toLowerCase() && 
         <>
           <Container children={<BannerInfos data={data} />} />
           <Container children={<TabZone user={data}/>} /> 
