083e1a865b224af397c0623534de23afd566bc89
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-09-08 15:53:16 +0200

    Update mod purchase

--- src/components/Modal/ModPurchase.js
+++ src/components/Modal/ModPurchase.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useContracts } from "../../context/ContractContext";
@@ -8,20 +8,23 @@ import { LoaderIcon } from "..";
 import { deleteIcon, checkmarkIcon } from "../../images";
 import classnames from "classnames";
 import { MarketplaceAddress } from "../../contracts/Marketplace";
-import { ethers } from "ethers"
+import { ethers } from "ethers";
 
-const ModPurchase = ({open, setOpen, sale, nft, user}) => {
+const ModPurchase = ({ open, setOpen, sale, nft, user }) => {
   const cancelButtonRef = useRef(null);
-  const { marketplace, xsro } = useContracts()
+  const { marketplace, xsro } = useContracts();
   const [isApproved, setIsApproved] = useState(false);
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   //Function to approve
   const handleApproveButton = async () => {
     try {
       setLoading(true);
-      const tx = await xsro.approve(MarketplaceAddress, ethers.utils.parseEther(sale.price));
+      const tx = await xsro.approve(
+        MarketplaceAddress,
+        ethers.utils.parseEther(sale.price)
+      );
       await tx.wait();
       setLoading(false);
       setIsApproved(true);
@@ -49,37 +52,37 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
     }
   };
 
-  const handleBuyButton = async() => {
+  const handleBuyButton = async () => {
     try {
-      setLoading(true)
-      const tx = await marketplace.buyNft(sale.saleId)
-      await tx.wait()
-      setLoading(false)
+      setLoading(true);
+      const tx = await marketplace.buyNft(sale.saleId);
+      await tx.wait();
+      setLoading(false);
       toast.success(`Nft bougth successfully \n`, {
-          position: "top-right",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: false,
-          draggable: true,
-          progress: undefined,
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
       });
       setTimeout(() => {
-        setOpen({...open, buyNft: false})
+        setOpen({ ...open, buyNft: false });
       }, 2000);
     } catch (e) {
-        setLoading(false)
-        toast.error(e.message, {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-        });
+      setLoading(false);
+      toast.error(e.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
     }
-  }
+  };
 
   return (
     <Transition.Root show={open.buyNft} as={Fragment}>
@@ -87,7 +90,7 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
         as="div"
         className="fixed z-10 inset-0 overflow-y-auto"
         initialFocus={cancelButtonRef}
-        onClose={() => setOpen({...open, buyNft: false})}
+        onClose={() => setOpen({ ...open, buyNft: false })}
       >
         <div className="flex justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
@@ -117,7 +120,7 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-          <div className="inline-block align-bottom bg-gray-900 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full border border-gray-600">
+            <div className="inline-block align-bottom bg-gray-900 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full border border-gray-600">
               <div className="bg-black bg-opacity-90 px-4 sm:p-6 ">
                 <div className="bg-gradient-to-b  flex justify-center items-center py-5">
                   <div className="rounded-lg">
@@ -126,7 +129,9 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                         <div className="text-white text-center">
                           <h2 className="text-5xl font-bold py-4">Checkout</h2>
                           <p className="text-sm font-bold">
-                            You are about to purchase <i style={{color: 'yellow'}}>{nft.title}</i> from {nft.author}
+                            You are about to purchase{" "}
+                            <i style={{ color: "yellow" }}>{nft.title}</i> from{" "}
+                            {nft.author}
                           </p>
                         </div>
                       </div>
@@ -141,32 +146,15 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                             Enter quantity. 1 available
                           </p>
                         </div>
-                        <div className="pt-5 px-7">
-                          <div className="flex">
-                            <select
-                              className="text-sm border rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap"
-                            >
-                              <option>XSRO</option>
-                              <option disabled>ETH (soon)</option>
-                              <option disabled>SRO (soon)</option>
-                            </select>
-                            <input
-                              type="text"
-                              className="flex-shrink flex-grow flex-auto leading-normal w-px bg-gray-900 text-white border border-gray-400 shadow-inner rounded-md py-3 px-4 focus:outline-none  focus:border-gray-500"
-                              placeholder="10"
-                            />
-                          </div>
-                          <p className="text-xs text-white pt-2">
-                            Price per edition
-                          </p>
-                        </div>
                       </div>
                       <div className="pt-5 ml-4 text-left text-white ">
                         <p>Balance : {user.balance.xsro} XSRO</p>
-                        <p>Service fee : {(sale.price * 0.025).toFixed(5)} XSRO</p>
+                        <p>
+                          Service fee : {(sale.price * 0.025).toFixed(5)} XSRO
+                        </p>
                         <p>Total Price : {sale.price} XSRO</p>
                       </div>
-                      
+
                       {/*Approve xsro*/}
 
                       <div className="flex items-center justify-center pt-4 pb-3">
@@ -177,7 +165,7 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                                 alt=""
                                 className="w-5 "
                                 src={checkmarkIcon}
-                                />
+                              />
                             </div>
                             <div className="pr-5">
                               <button
@@ -185,8 +173,8 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                                 className={classnames(
                                   "bg-green-100 rounded-xl",
                                   "text-black px-8 py-3"
-                                  )}
-                                  >
+                                )}
+                              >
                                 Appoved
                               </button>
                             </div>
@@ -200,9 +188,9 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                                 className={classnames(
                                   "transition duration-300 bg-gradient-to-br rounded-xl hover:opacity-75",
                                   "text-black px-8 py-3 from-primary-200 to-primary-200" ||
-                                  "text-white hover:text-primary-200"
-                                  )}
-                                  >
+                                    "text-white hover:text-primary-200"
+                                )}
+                              >
                                 In progress...
                               </button>
                             </div>
@@ -216,14 +204,14 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                                   alt=""
                                   className="w-4 mr-2"
                                   src={deleteIcon}
-                                  />{" "}
+                                />{" "}
                               </div>
                             )}
                             <div className="">
                               <ButtonOnClick
                                 onClick={handleApproveButton}
                                 buttonStyle
-                                >
+                              >
                                 Approve xsro
                               </ButtonOnClick>
                             </div>
@@ -234,8 +222,9 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                       {/*Buy NFT*/}
 
                       <div className="flex items-center justify-center pt-4 pb-3">
-                        {isApproved ? loading ? (
-                          <div className="flex items-center justify-center pt-4 pb-3">
+                        {isApproved ? (
+                          loading ? (
+                            <div className="flex items-center justify-center pt-4 pb-3">
                               <LoaderIcon />
                               <div className="">
                                 <button
@@ -243,9 +232,9 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                                   className={classnames(
                                     "transition duration-300 bg-gradient-to-br rounded-xl hover:opacity-75",
                                     "text-black px-8 py-3 from-primary-200 to-primary-200" ||
-                                    "text-white hover:text-primary-200"
-                                    )}
-                                    >
+                                      "text-white hover:text-primary-200"
+                                  )}
+                                >
                                   In progress...
                                 </button>
                               </div>
@@ -259,30 +248,29 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                                     alt=""
                                     className="w-7 "
                                     src={deleteIcon}
-                                    />{" "}
+                                  />{" "}
                                 </div>
                               )}
                               <div className=" ">
                                 <ButtonOnClick
                                   onClick={handleBuyButton}
                                   buttonStyle
-                                  >
-                                  Proceed to payment
+                                >
+                                  Buy for {sale.price} XSRO
                                 </ButtonOnClick>
                               </div>
                             </>
                           )
-                          :
-                          (
-                            <div className="">
+                        ) : (
+                          <div className="">
                             <button
                               disabled={true}
                               className={classnames(
                                 "bg-gray-200 rounded-xl",
                                 "text-black px-8 py-3 from-primary-200 to-primary-200"
-                                )}
-                                >
-                              Proceed to payment
+                              )}
+                            >
+                              Buy for {sale.price} XSRO
                             </button>
                           </div>
                         )}
@@ -290,7 +278,7 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                       <div className="flex items-center justify-center pt-3 pb-3">
                         <a
                           href="#fs-sale"
-                          onClick={() => setOpen({...open, buyNft: false})}
+                          onClick={() => setOpen({ ...open, buyNft: false })}
                           className="  px-5 py-3 text-center text-white hover:bg-gray-200 hover:text-black font-bold rounded-lg text-sm"
                         >
                           Cancel
@@ -300,13 +288,12 @@ const ModPurchase = ({open, setOpen, sale, nft, user}) => {
                   </div>
                 </div>
               </div>
-          </div>
+            </div>
           </Transition.Child>
         </div>
       </Dialog>
     </Transition.Root>
   );
-}
+};
 
-
-export default ModPurchase
+export default ModPurchase;
