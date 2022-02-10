5e8e2a80ff65b3404d86110a6ebc67dafee56cb4
johnfr14 <jonathan.alyra@gmail.com>
2022-02-10 18:38:35 +0900

    available for non wallet also

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -9,8 +9,8 @@ export const UserContext = createContext()
 
 const initialState = { 
   data: {
-    fullAddress: null,
-    address: null,
+    fullAddress: '0x0000000000000000000000000000000000000000',
+    address: '0x00...0000',
     name: null,
     bio: null,
     url: null,
